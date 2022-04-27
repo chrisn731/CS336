6391477f9c70c4433546cf7a5a75a33c840d517e
Timothy Walker <tpw32@scarletmail.rutgers.edu>
2022-04-27 18:30:30 -0400

    db update

--- create_db.sql
+++ create_db.sql
@@ -6,3 +6,36 @@ CREATE TABLE users(
 	username VARCHAR(30) PRIMARY KEY,
     password VARCHAR(30)
 );
+
+CREATE TABLE listings(
+	l_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    itemname VARCHAR(30),
+    subcategory VARCHAR(30),
+    subattribute VARCHAR(30),
+    price DECIMAL(10,2),
+    minsale DECIMAL(10,2)
+	
+);
+
+#USER POSTS LISTING
+CREATE TABLE posts(
+	l_id int PRIMARY KEY,
+	username VARCHAR(30),
+    FOREIGN KEY(username) REFERENCES users(username),
+    FOREIGN KEY(l_id) REFERENCES listings(l_id)
+);
+
+CREATE TABLE bids(
+	b_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	price DECIMAL(10,2),
+    dtime datetime
+);
+
+#Bids on listings
+CREATE TABLE bidson(
+	b_id int PRIMARY KEY,
+	l_id int,
+    FOREIGN KEY(l_id) REFERENCES listings(l_id),
+    FOREIGN KEY(b_id) REFERENCES bids(b_id)
+);
+
